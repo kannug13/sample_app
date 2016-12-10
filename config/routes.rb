ce41922d6b49211060a09c290699b1a2581ddfb6
kannug13 <kannug13@gmail.com>
2016-12-10 11:03:42 +0000

    initial_

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_page/home'
+
+  get 'static_page/help'
+
+  get 'static-page/home'
+  get 'static-page/help'
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
